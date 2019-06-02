9b8f92ee6afd7bf096b0e7b6f2bd84085d2a14b3
Dan Palmer <dan@thread.com>
2019-06-02 13:06:44 +0100

    Support null component

--- src/app.lua
+++ src/app.lua
@@ -30,8 +30,7 @@ local sp_name = page_data["page"]["name"]
 local sp_desc = in_data["page"]["status_description"]
 local sp_link = page_data["page"]["url"]
 local sp_indc = in_data["page"]["status_indicator"]
-local sp_comp = in_data["component"]["name"]
-local sp_comp_desc = in_data["component"]["description"]
+local sp_comp = in_data["component"]
 
 local domain = "statuspage-slack-proxy.herokuapp.com"
 local by_line = "Forwarded by <https://" .. domain .. "|" .. domain .. ">"
@@ -41,10 +40,10 @@ local footer = by_line .. " – " .. unsubscribe
 local out_data = {
     attachments = {
         {
-            fallback = sp_name .. " is experiencing a " .. sp_desc .. " in component '" .. sp_comp .. "'",
+            fallback = sp_name .. " is experiencing a " .. sp_desc,
             color = colours[sp_indc],
             author_name = sp_name,
-            title = sp_desc .. " in " .. sp_comp .. " component",
+            title = sp_desc,
             title_link = sp_link,
             fields = {
                 {
@@ -52,26 +51,43 @@ local out_data = {
                     value = sp_name,
                     short = true,
                 },
-                {
-                    title = "Component",
-                    value = sp_comp,
-                    short = true,
-                },
+                
             },
             footer = footer,
         },
     },
 }
 
-if not (sp_comp_desc == json.null or sp_comp_desc == '')   then
+if sp_comp ~= json.null then
+    local sp_comp_name = sp_comp["name"]
+    local sp_comp_desc = sp_comp["description"]
+
+    out_data["attachments"][1]["fallback"] = (
+        sp_name .. " is experiencing a " .. sp_desc .. " in component '" .. sp_comp_name .. "'"
+    )
+    out_data["attachments"][1]["title"] = (
+        sp_desc .. " in " .. sp_comp_name .. " component"
+    )
+
     table.insert(
         out_data["attachments"][1]["fields"],
         {
-            title = "Description",
-            value = sp_comp_desc,
-            short = false,
+            title = "Component",
+            value = sp_comp_name,
+            short = true,
         }
     )
+
+    if not (sp_comp_desc == json.null or sp_comp_desc == '')   then
+        table.insert(
+            out_data["attachments"][1]["fields"],
+            {
+                title = "Description",
+                value = sp_comp_desc,
+                short = false,
+            }
+        )
+    end
 end
 
 local out_json = json.encode(out_data)
